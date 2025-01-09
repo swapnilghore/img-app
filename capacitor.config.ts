c296dce866febbf5c1fc642ddd795a00d561139c
swapnilghore <swapnilghore55@gmail.com>
2025-01-09 15:59:13 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'img-app',
+  webDir: 'www'
+};
+
+export default config;
